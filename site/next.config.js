1fb7f76479e96c69db96dca2a16fadd420c97bd1
kian <kmaranon9@gmail.com>
2022-03-15 19:42:38 -0700

    Testing subdomains

--- site/next.config.js
+++ site/next.config.js
@@ -1,24 +1,6 @@
-const  DOCS_URL  = process.env.DOCS_URL ?? "https://kian-codes-docs.vercel.app/"
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: `/:path*`,
-      },
-      {
-        source: '/docs',
-        destination: `${DOCS_URL}/docs`,
-      },
-      {
-        source: '/blog/:path*',
-        destination: `${DOCS_URL}/docs/:path*`,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
