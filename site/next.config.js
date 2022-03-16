018344b552600527eb63a12ba2165d7ce1181656
kian <kmaranon9@gmail.com>
2022-03-15 17:40:29 -0700

    Set up for multizones

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,25 @@
+const { DOCS_URL } = process.env
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig, {
+  async rewrites() {
+    return[
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/docs',
+        destination: `${DOCS_URL}/docs`,
+      },
+      {
+        source: '/blog/:path*',
+        destination: `${DOCS_URL}/docs/:path*`,
+      },
+    ]
+  }
+}
